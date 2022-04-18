dc6a38e8996f9f2850c7791721f18815661348fd
Eric Dallo <ericdallo06@hotmail.com>
2022-04-17 21:12:32 -0300

    Add sample project

---
+++ lib/cljd-out/clojuredart/sample.dart
@@ -0,0 +1,11 @@
+import "dart:core" as dc;
+import "sample.dart" as hgdcslcoc_sample;
+import "../cljd/core.dart" as hgdcslcoc_core;
+import "package:flutter/material.dart" as f_material;
+
+// BEGIN main
+dc.dynamic main(){
+return (hgdcslcoc_core.print.$_invoke$1("hello, world\n", ));
+}
+
+// END main
